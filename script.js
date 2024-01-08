c0d1d6db2e1ffe5383c5f297a4ecbbc0dde5201c
sanjaraiy <deepakrai78788@gmail.com>
2024-01-09 02:57:07 +0530

    Image Search Engine

---
+++ script.js
@@ -0,0 +1,48 @@
+const accessKey='OtWSkeNBZmQ6czlw2FjKnxj9tg56dBK6DGy-hQwFi0U';
+
+
+const form=document.querySelector('#form');
+const search_box=document.querySelector('#search-box');
+const search_result=document.querySelector('#search-result');
+const show_More=document.querySelector('#show-more');
+
+let keyword="";
+let page=1;
+
+async function searchImage(){
+    keyword=search_box.value;
+    const url=`https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+
+    const response=await fetch(url);
+    const data=await response.json();
+
+      if(page===1){
+        search_result.innerHTML="";
+      }
+    // console.log(data);
+    const results=data.results;
+
+    results.map((result)=>{
+        const image=document.createElement('img');
+        image.src=result.urls.small;
+        const imageLink=document.createElement('a');
+        imageLink.href=result.links.html;
+        imageLink.target='_blank';
+        imageLink.appendChild(image);
+        search_result.appendChild(imageLink);
+    })
+
+    show_More.style.display='flex';
+
+}
+
+form.addEventListener('submit',(event)=>{
+    event.preventDefault();
+    page=1;
+    searchImage();
+},false)
+
+show_More.addEventListener('click',()=>{
+    page++;
+    searchImage();
+},false)
